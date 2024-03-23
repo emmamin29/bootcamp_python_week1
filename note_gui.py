fa3203c5de8b7f37e561c95e4745cf6da7785f51
emmamin29 <bookworm1129@naver.com>
2024-03-24 00:17:10 +0900

    수정

--- note_gui.py
+++ note_gui.py
@@ -5,7 +5,8 @@ from tkinter.filedialog import *
 def new_file():
     #텍스트 영역 지우기
     text_area.delete(1.0, END)
-    
+
+#파일 저장하기
 def save_file():
     f = asksaveasfile(mode='w', defaultextension=".txt", filetypes=[('Text files', '.txt')])
     text_save = str(text_area.get(1.0, END))
